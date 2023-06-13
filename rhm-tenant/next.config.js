f17661cbb519fe359d16b7c8939d2f64b51b5254
unknown <anhnguyentuan8@gmail.com>
2023-06-13 17:19:01 +0700

    update pwa

--- rhm-tenant/next.config.js
+++ rhm-tenant/next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const withLess = require("next-with-less");
+const withPWA = require("next-pwa");
 module.exports = withLess({
   lessLoaderOptions: {
     lessOptions: {
       modifyVars: {
-        'primary-color': "#769FCD",
-        'secondary-color': "#B9D7EA",
-        'success-color': "#7ADD86",
-        'error-color': "#FA5556",
-        'bg-base-color': "#D6E6F2"
+        "primary-color": "#769FCD",
+        "secondary-color": "#B9D7EA",
+        "success-color": "#7ADD86",
+        "error-color": "#FA5556",
+        "bg-base-color": "#D6E6F2",
       },
     },
   },
@@ -16,5 +17,11 @@ module.exports = withLess({
     styledComponents: true,
   },
   reactStrictMode: true,
+});
 
+module.exports = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
